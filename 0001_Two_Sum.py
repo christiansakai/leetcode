0906c4d40941bdd52400c9e31358490c01b89a39
Christian Sakai <christianmsakai@gmail.com>
2020-03-04 01:17:08 -0500

    Fizz Buzz

--- 0001_Two_Sum.py
+++ 0001_Two_Sum.py
@@ -3,6 +3,7 @@ from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashMap = {}
+
         for i in range(len(nums)):
             num = nums[i]
             remaining = target - num
