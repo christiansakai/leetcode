c83a7d6a0ae0c7d1b3614cccab8785333f972d2a
SAKAI, CHRISTIAN (CORP) <CHRISTIAN.SAKAI@adp.com>
2020-03-04 11:08:16 -0500

    Plus One

---
+++ 0066_Plus_One.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        carry = 1
+
+        for i in range(len(digits) - 1, -1, -1):
+            total = digits[i] + carry
+
+            if total == 10:
+                digits[i] = 0
+                carry = 1
+            else:
+                digits[i] = total
+                carry = 0
+
+        if carry == 1:
+            digits.insert(0, 1)
+
+        return digits
